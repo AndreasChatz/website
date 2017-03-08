6bb757b836b70846c814768a21cef57e3a939f27
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-03-08 16:52:25 +0200

    Create admin and add Albums to admin page

--- music/models.py
+++ music/models.py
@@ -8,6 +8,9 @@ class Album(models.Model):
 	genre = models.CharField(max_length=50)
 	album_logo = models.CharField(max_length=250)
 
+	def __str__(self):
+		return self.artist + '-' + self.album_title
+
 
 class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
