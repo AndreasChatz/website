f205da513e10e208929ead8bd320bc723cbc5c9c
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-03-08 14:19:33 +0200

    Add Album model

--- music/models.py
+++ music/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+	artist = models.CharField(max_length=40)
+	album_title = modles.CharField(max_length=100)
+	genre = models.CharField(max_length=50)
+	album_logo = models.CharField(max_length=250)
+	
